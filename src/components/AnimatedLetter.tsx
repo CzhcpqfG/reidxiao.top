13556e1e761813404590e1362532b0f8759e5299
Reid <1592775514@qq.com>
2026-08-02 10:55:19 +0800

    Add src/components/AnimatedLetter.tsx

---
+++ src/components/AnimatedLetter.tsx
@@ -0,0 +1,17 @@
+import { motion, useTransform, type MotionValue } from 'framer-motion'
+
+interface AnimatedLetterProps {
+  char: string
+  scrollYProgress: MotionValue<number>
+  index: number
+  total: number
+}
+
+export function AnimatedLetter({ char, scrollYProgress, index, total }: AnimatedLetterProps) {
+  const charProgress = index / total
+  const start = charProgress - 0.1
+  const end = charProgress + 0.05
+  const opacity = useTransform(scrollYProgress, [start, end], [0.2, 1])
+
+  return <motion.span style={{ opacity }}>{char}</motion.span>
+}
